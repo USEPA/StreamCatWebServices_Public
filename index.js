07c080e26f44dab298e4fa00fe5f088cb5bc4c2b
Travis Hudson <Hudson.Travis@epa.gov>
2024-04-11 14:44:36 -0700

    Fixed swagger yml and changed name for future proofing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { SwaggerUIBundle } from "swagger-ui-dist"
 window.onload = () => {
     window.ui = SwaggerUIBundle({
-          url: 'swagger_update.yml',
+          url: 'swagger.yml',
           dom_id: '#ui',
     });
 };
