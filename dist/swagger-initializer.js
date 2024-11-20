06dd28f45c29860d7b29613126736d77d9c1d2e5
Travis Hudson <Hudson.Travis@epa.gov>
2024-11-19 17:16:54 -0800

    Fixing module not importing correctly error

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,21 +1,52 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+import { SwaggerUIBundle } from "swagger-ui-dist";
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    url: "streamcat_swagger.yml",
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
-
-  //</editor-fold>
+const loadSwaggerUI = (url) => {
+    window.ui = SwaggerUIBundle({
+        url: url,
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIBundle.presets.apis.standaloneLayout
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+    });
 };
+
+document.getElementById('streamcat-link').addEventListener('click', () => {
+    loadSwaggerUI('streamcat_swagger.yml');
+});
+
+document.getElementById('lakecat-link').addEventListener('click', () => {
+    loadSwaggerUI('lakecat_swagger.yml');
+});
+
+// Load Streamcat by default
+window.onload = () => {
+    loadSwaggerUI('streamcat_swagger.yml');
+};
+
+// window.onload = function() {
+//   //<editor-fold desc="Changeable Configuration Block">
+
+//   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+//   window.ui = SwaggerUIBundle({
+//     url: "streamcat_swagger.yml",
+//     dom_id: '#swagger-ui',
+//     deepLinking: true,
+    
+//     presets: [
+//       SwaggerUIBundle.presets.apis,
+//       SwaggerUIStandalonePreset
+//     ],
+//     plugins: [
+//       SwaggerUIBundle.plugins.DownloadUrl
+//     ],
+//     layout: "StandaloneLayout"
+//   });
+
+//   //</editor-fold>
+// };
